aaf9e34edcfff9d05621d25652e18a0251c5808a
Esmael Abdlkadr <esmaelabdlkadr00@gmail.com>
2024-05-02 23:55:29 +0300

    vercel  route added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import STT from "./STT";
 import TextToVoice from "./TTS";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout/Layout.jsx";
+import Layout from "./Layout/Layout";
 
 function App() {
   return (
